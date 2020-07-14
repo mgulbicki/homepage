8c45d64eb220e7980ee7370cd5f961ffb2f15441
Marek Gulbicki <mgulbicki73@gmail.com>
2020-07-14 08:11:53 +0200

    refactor using sass

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
-const name = 'Marek';
+const firstName = "Marek";
 const age = 47;
 
-console.log(`Witaj, nazywam się ${name} i mam ${age} lat.`);
+console.log(
+  `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`
+);
+
+const emptyParagraph = document.querySelector(".week-summary__description--js");
+
+emptyParagraph.innerHTML = "Nawet uzupełniłem treść javascriptem!";
+
+const greet = (age, firstName) => {
+  console.log(
+    `Witaj Drogi Odwiedzający! Nazywam się ${firstName} i mam ${age} lat.`
+  );
+};
+
+greet(47, "Marek");
+
+const myNumber = 10;
+
+switch (myNumber) {
+  case 7:
+    console.log("jestem siódemką");
+    break;
+  case 9:
+    console.log("jestem dziewiątką");
+    break;
+  default:
+    console.log("jestem czymś innym");
+}
+
+const result = 23 < 32 ? true : false;
+
+console.log(result);
+
+const button = document.querySelector(".action--js");
+
+console.log(button);
+
+button.addEventListener("click", () => {
+  const heading = document.querySelector(".main__heading--js");
+  heading.innerHTML = "Witaj Drogi Odwiedzający, nazywam się Marek";
+  console.log(heading.classList.contains("main__heading-2"));
+});
+
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
